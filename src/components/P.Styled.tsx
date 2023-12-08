96828081f11720b1c0c06eecd27ca01d7a8e1741
Vladislav <labkovich.vl@gmail.com>
2023-12-08 18:36:47 +0300

    feat: add adaptive image

--- src/components/P.Styled.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-
-export const StyledP = styled.p`
-  color: #ABB3BA;
-  font-size: 12px;
-  font-weight: 500;
-  margin: 0px 20px 23px 20px;
-`
--- src/components/Title.Styled.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const StyledTitle = styled.h2`
-  display: flex;
-  color: black;
-  font-size: 16px;
-  font-weight: 700;
-  margin: 20px 210px 20px 20px;
-`
