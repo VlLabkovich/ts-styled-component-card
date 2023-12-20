82811493a1fe6aaf1a41737fa1b60a795badb1dc
Vladislav <labkovich.vl@gmail.com>
2023-12-21 00:08:44 +0300

    feat: add props

--- src/components/Button.styled.tsx
+++ src/components/Button.styled.tsx
@@ -1,46 +1,46 @@
 import styled, {css} from "styled-components";
 
 export const StyledBtnFlex = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 12px;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 12px;
+    padding: 0 10px;
 `
 
 type StyledBtnPropsType = {
-    background: string
-    color: string
     btnType: "primary" | "outlined"
 }
 
 export const StyledBtn = styled.button<StyledBtnPropsType>`
-  width: 86px;
-  height: 30px;
-  color: ${props => props.color};
-  border-radius: 5px;
-  font-family: 'Inter', sans-serif;
-  font-size: 10px;
-  font-weight: 700;
-  flex-shrink: 0;
-  
-  // primary
-  ${props => props.btnType === "primary" && css<StyledBtnPropsType>`
-    background-color: ${props => props.background};
-    border: 0;
+    width: 86px;
+    height: 30px;
+    border-radius: 5px;
+    font-family: 'Inter', sans-serif;
+    font-size: 10px;
+    font-weight: 700;
 
-    &:hover {
-      background-color: #64ce94;
-    }
-  `
-  };
-  // outlined
-  ${props => props.btnType === "outlined" && css<StyledBtnPropsType>`
-    background-color: ${props => props.background};
-    border: 2px solid #4E71FE;
+    // primary
+    ${props => props.btnType === "primary" && css<StyledBtnPropsType>`
+        background-color: #4E71FE ;
+        color: #FFF;
+        border: 0;
 
-    &:hover {
-      border-color: #64ce94;
-      color: #64ce94;
-    }
-  `
-  };
+        &:hover {
+            background-color: #64ce94;
+        }
+    `
+    };
+    
+    // outlined
+    ${props => props.btnType === "outlined" && css<StyledBtnPropsType>`
+        background-color: #FFF;
+        color: #4E71FE;
+        border: 2px solid #4E71FE;
+        
+        &:hover {
+            border-color: #64ce94;
+            color: #64ce94;
+        }
+    `
+    };
 `
