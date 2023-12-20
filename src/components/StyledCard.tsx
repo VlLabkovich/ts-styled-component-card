886e71ffc10eaf3a75d3ee00a1532105176ec3fe
Vladislav <labkovich.vl@gmail.com>
2023-12-21 00:09:51 +0300

    feat: add component

---
+++ src/components/StyledCard.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const StyledCard = styled.div`
+    display: flex;
+    flex-direction: column;
+    max-width: 300px;
+    min-height: 350px;
+    border-radius: 15px;
+    background-color: #FFF;
+    box-shadow: 0 4px 20px 5px rgba(0, 0, 0, 0.10);
+    padding: 10px 10px 22px 10px;
+`
