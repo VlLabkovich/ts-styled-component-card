1bd06918bd7ef46aac15a95fb48fe4ae2a9c3a20
Vladislav <labkovich.vl@gmail.com>
2023-12-21 00:10:10 +0300

    feat: add component

---
+++ src/components/StyledText.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledText = styled.p`
+    color: #ABB3BA;
+    font-size: 12px;
+    font-weight: 500;
+    line-height: 20px;
+    margin: 0 10px 19px 10px;
+`
