e29040b61e7d0f893ae971bdb12e90c4a0a68f21
Vladislav <labkovich.vl@gmail.com>
2023-12-21 00:10:24 +0300

    feat: add component

---
+++ src/components/StyledTitle.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledTitle = styled.h2`
+    color: black;
+    font-size: 16px;
+    font-weight: 700;
+    line-height: normal;
+    margin: 20px 10px;
+`
